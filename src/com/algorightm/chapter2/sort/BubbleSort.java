34d9c321e987ed2524c62238a9d3dd7bc4ac8406
vxiaohan <xhdtc@126.com>
2018-01-07 21:48:11 +0800

    Finish bubble sort

---
+++ src/com/algorightm/chapter2/sort/BubbleSort.java
@@ -0,0 +1,17 @@
+package com.algorightm.chapter2.sort;
+
+public class BubbleSort {
+    public static void sort(Comparable[] data) {
+        for (int i = 1; i < data.length; i++) {
+            Comparable temp = data[i];
+            for (int j = i - 1; j >= 0; j--) {
+                if (temp.compareTo(data[j]) < 0) {
+                    data[j + 1] = data[j];
+                    data[j] = temp;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+}
--- src/com/algorightm/chapter2/sort/SortTest.java
+++ src/com/algorightm/chapter2/sort/SortTest.java
@@ -4,60 +4,79 @@ package com.algorightm.chapter2.sort;
  * @author Xiao Han
  */
 public class SortTest {
-    public static boolean isSorted(Comparable[] a){
-        for (int i=0;i<a.length-1;i++){
-            if (a[i].compareTo(a[i+1])>0){
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            if (a[i].compareTo(a[i + 1]) > 0) {
                 return false;
             }
         }
         return true;
     }
-    public static void show(Comparable[] a){
-        for (int i=0;i<a.length;i++){
+
+    public static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
             System.out.print(a[i]);
-            if (i != a.length -1){
+            if (i != a.length - 1) {
                 System.out.print(",");
             }
         }
         System.out.print("\n");
     }
-    public static boolean isEqual(Comparable[] a, Comparable[] b){
+
+    public static boolean isEqual(Comparable[] a, Comparable[] b) {
         if (a.length != b.length) {
             return false;
         }
-        for (int i=0; i<a.length; i++){
-            if (a[i].compareTo(b[i])!=0){
+        for (int i = 0; i < a.length; i++) {
+            if (a[i].compareTo(b[i]) != 0) {
                 return false;
             }
         }
         return true;
     }
+
     @org.junit.Test
     public void selectionSort() {
-        Integer[] a = {2,3,1,5,6,3};
+        Integer[] a = {2, 3, 1, 5, 6, 3};
         SelectionSort.sort(a);
-        Integer[] result = {1,2,3,3,5,6};
+        Integer[] result = {1, 2, 3, 3, 5, 6};
         assert isEqual(a, result);
         assert isSorted(a);
         Integer[] b = {};
         SelectionSort.sort(b);
-        assert  isSorted(b);
+        assert isSorted(b);
         Integer[] c = {1};
         SelectionSort.sort(c);
-        assert  isSorted(c);
+        assert isSorted(c);
     }
+
     @org.junit.Test
     public void insertSort() {
-        Integer[] a = {2,3,1,5,6,3};
+        Integer[] a = {2, 3, 1, 5, 6, 3};
         InsertSort.sort(a);
-        Integer[] result = {1,2,3,3,5,6};
+        Integer[] result = {1, 2, 3, 3, 5, 6};
         assert isEqual(a, result);
         assert isSorted(a);
         Integer[] b = {};
         InsertSort.sort(b);
-        assert  isSorted(b);
+        assert isSorted(b);
         Integer[] c = {1};
         InsertSort.sort(c);
-        assert  isSorted(c);
+        assert isSorted(c);
+    }
+
+    @org.junit.Test
+    public void bubbleSort() {
+        Integer[] a = {2, 3, 1, 5, 6, 3};
+        BubbleSort.sort(a);
+        Integer[] result = {1, 2, 3, 3, 5, 6};
+        assert isEqual(a, result);
+        assert isSorted(a);
+        Integer[] b = {};
+        BubbleSort.sort(b);
+        assert isSorted(b);
+        Integer[] c = {1};
+        BubbleSort.sort(c);
+        assert isSorted(c);
     }
 }
