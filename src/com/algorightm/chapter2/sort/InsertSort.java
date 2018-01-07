3f225e55a3bac8716ca1e3abfc2ca89a70c3b6be
vxiaohan <xhdtc@126.com>
2018-01-07 21:46:32 +0800

    Modify format

--- src/com/algorightm/chapter2/sort/InsertSort.java
+++ src/com/algorightm/chapter2/sort/InsertSort.java
@@ -4,26 +4,29 @@ package com.algorightm.chapter2.sort;
  * @author Xiao Han
  */
 public class InsertSort {
-    static void sort(Comparable[] a){
-        for (int i=1;i<a.length;i++){
+    private static void move(Comparable[] a, int sourceIndex, int desIndex) {
+        Comparable temp = a[sourceIndex];
+        for (int k = sourceIndex; k > desIndex; k--) {
+            a[k] = a[k - 1];
+        }
+        a[desIndex] = temp;
+    }
+
+    static void sort(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
             int insertPosition = i;
-            if (a[i].compareTo(a[0])<=0){
+            if (a[i].compareTo(a[0]) <= 0) {
                 insertPosition = 0;
-            }
-            else {
+            } else {
                 for (int j = i - 1; j > 0; j--) {
-                    if (a[i].compareTo(a[j]) <= 0 && a[i].compareTo(a[j-1])>=0) {
+                    if (a[i].compareTo(a[j]) <= 0 && a[i].compareTo(a[j - 1]) >= 0) {
                         insertPosition = j;
                         break;
                     }
                 }
             }
-            if (insertPosition != i){
-                Comparable temp = a[i];
-                for (int k=i;k>insertPosition;k--){
-                    a[k] = a[k-1];
-                }
-                a[insertPosition] = temp;
+            if (insertPosition != i) {
+                InsertSort.move(a, i, insertPosition);
             }
         }
     }
--- src/com/algorightm/chapter2/sort/SelectionSort.java
+++ src/com/algorightm/chapter2/sort/SelectionSort.java
@@ -4,13 +4,13 @@ package com.algorightm.chapter2.sort;
  * @author Xiao Han
  */
 public class SelectionSort {
-    public static void sort(Comparable[] a){
+    public static void sort(Comparable[] a) {
         int length = a.length;
-        for (int i=0;i<length;i++){
+        for (int i = 0; i < length; i++) {
             // Find min index
             int minIndex = i;
-            for (int j=i+1;j<length;j++){
-                if (a[j].compareTo(a[minIndex]) < 0){
+            for (int j = i + 1; j < length; j++) {
+                if (a[j].compareTo(a[minIndex]) < 0) {
                     minIndex = j;
                 }
             }
