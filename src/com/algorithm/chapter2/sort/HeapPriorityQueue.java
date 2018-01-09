4ccd68024121f815827cbb214a75fcd77e658136
vxiaohan <xhdtc@126.com>
2018-01-09 11:11:21 +0800

    Finish heap

--- src/com/algorithm/chapter2/sort/HeapPriorityQueue.java
+++ src/com/algorithm/chapter2/sort/HeapPriorityQueue.java
@@ -8,6 +8,10 @@ public class HeapPriorityQueue<Key extends Comparable<Key>> {
     private Key[] heap;
     private int number = 0;
 
+    public HeapPriorityQueue() {
+        heap = (Key[]) new Comparable[11];
+    }
+
     public HeapPriorityQueue(int maxNumber) {
         heap = (Key[]) new Comparable[maxNumber + 1];
     }
@@ -16,23 +20,44 @@ public class HeapPriorityQueue<Key extends Comparable<Key>> {
         return number;
     }
 
-    public void insert(Key data) {
-        number++;
-        heap[number] = data;
-        swim(number);
+    public Key insert(Key data) {
+        if (number>0 && !data.getClass().equals(heap[1].getClass()) ){
+            return null;
+        }else {
+            number++;
+            if (number == heap.length) {
+                reSize();
+            }
+            heap[number] = data;
+            swim(number);
+            return data;
+        }
+    }
+
+    public void reSize() {
+        Key[] newHeap = (Key[]) new Comparable[number * 2];
+        for (int i = 0; i < heap.length; i++) {
+            newHeap[i] = heap[i];
+        }
+        this.heap = newHeap;
     }
 
     public Key delMax() {
-        Key maxValue = heap[1];
-        exchange(1, number);
-        heap[number] = null;
-        number--;
-        sink(1);
-        return maxValue;
-    }
-
-    public Key[] getHeap() {
-        return heap;
+        if (number > 1) {
+            Key maxValue = heap[1];
+            exchange(1, number);
+            heap[number] = null;
+            number--;
+            sink(1);
+            return maxValue;
+        } else if (number == 1) {
+            Key maxValue = heap[1];
+            heap[1] = null;
+            number--;
+            return maxValue;
+        }else{
+            return null;
+        }
     }
 
     private void exchange(int index1, int index2) {
@@ -42,10 +67,18 @@ public class HeapPriorityQueue<Key extends Comparable<Key>> {
     }
 
     private void swim(int index) {
-        while (index >= 1 && heap[index / 2].compareTo(heap[index]) < 0) {
+        while (index >= 1 && index / 2 >= 1 && heap[index / 2].compareTo(heap[index]) < 0) {
             exchange(index, index / 2);
             index = index / 2;
         }
+    }
+
+    private void showHeap() {
+        System.out.print("Number:" + number + "-");
+        for (int i = 0; i <= this.number; i++) {
+            System.out.print(heap[i] + ",");
+        }
+        System.out.println("");
     }
 
     private void sink(int index) {
@@ -58,6 +91,9 @@ public class HeapPriorityQueue<Key extends Comparable<Key>> {
                     if (heap[leftIndex].compareTo(heap[rightIndex]) < 0) {
                         exchange(index, rightIndex);
                         index = rightIndex;
+                    }else {
+                        exchange(index, leftIndex);
+                        index = leftIndex;
                     }
                 } else {
                     break;
@@ -71,5 +107,19 @@ public class HeapPriorityQueue<Key extends Comparable<Key>> {
                 }
             }
         }
+    }
+
+    public boolean isMaxHeap() {
+        int index = 1;
+        while (index * 2 <= number) {
+            if (heap[index].compareTo(heap[index * 2]) < 0) {
+                return false;
+            }
+            if (index * 2 + 1 <= number && heap[index].compareTo(heap[index * 2 + 1]) < 0) {
+                return false;
+            }
+            index++;
+        }
+        return true;
     }
 }
--- src/com/algorithm/chapter2/sort/HeapPriorityQueueTest.java
+++ src/com/algorithm/chapter2/sort/HeapPriorityQueueTest.java
@@ -1,13 +1,37 @@
 package com.algorithm.chapter2.sort;
 
 public class HeapPriorityQueueTest {
-    private boolean isMaxHeap(Comparable[] data) {
-        int level = 1;
-        while ()
-    }
-
     @org.junit.Test
     public void heapPriorityQueueTest() {
+        HeapPriorityQueue heap = new HeapPriorityQueue(7);
+        for (int i = 1; i <= 7; i++) {
+            heap.insert(i);
+        }
+        assert heap.size() == 7;
+        assert heap.isMaxHeap();
+        assert (Integer) heap.delMax() == 7;
+        assert heap.size() == 6;
+        for (int i = 10; i <= 27; i++) {
+            heap.insert(i);
+        }
+        assert heap.size() == 24;
+        assert heap.isMaxHeap();
 
+        HeapPriorityQueue heap2 = new HeapPriorityQueue();
+        for (int i = 1; i <= 7; i++) {
+            heap2.insert(i);
+        }
+        assert heap2.size() == 7;
+        assert heap2.isMaxHeap();
+        assert (Integer) heap2.delMax() == 7;
+        assert heap2.size() == 6;
+        for (int i = 10; i <= 27; i++) {
+            heap2.insert(i);
+        }
+        assert heap2.size() == 24;
+        assert heap2.isMaxHeap();
+        for (int i = 1; i <= 100; i++) {
+            heap2.delMax();
+        }
     }
 }
