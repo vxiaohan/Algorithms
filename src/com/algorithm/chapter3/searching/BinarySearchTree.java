712b7c41d632bafa9b981a49396c6a8c3d9a16e9
vxiaohan <xhdtc@126.com>
2018-01-15 20:12:40 +0800

    Finish binary search tree

--- src/com/algorithm/chapter3/searching/BinarySearchTree.java
+++ src/com/algorithm/chapter3/searching/BinarySearchTree.java
@@ -1,5 +1,7 @@
 package com.algorithm.chapter3.searching;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.Stack;
 
 /**
@@ -25,7 +27,11 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
     private Node root = null;
 
     public int size() {
-        return size(root);
+        if (root != null) {
+            return size(root);
+        } else {
+            return 0;
+        }
     }
 
     private int size(Node node) {
@@ -71,7 +77,7 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
                 break;
             }
             compareResult = tempNode.key.compareTo(key);
-            if (tempNode != fatherNodesStack.peek()){
+            if (tempNode != fatherNodesStack.peek()) {
                 fatherNodesStack.push(tempNode);
             }
             if (compareResult == 0) {
@@ -90,111 +96,292 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
         }
     }
 
-    public Key min(){
+    public Key min() {
         Node temp = root;
-        while (temp != null){
-            if (temp.left == null){
+        while (temp != null) {
+            if (temp.left == null) {
                 return temp.key;
-            }else{
+            } else {
                 temp = temp.left;
             }
         }
         return null;
     }
 
-    public Key max(){
+    public Key max() {
         Node temp = root;
-        while (temp != null){
-            if (temp.right == null){
+        while (temp != null) {
+            if (temp.right == null) {
                 return temp.key;
-            }else{
+            } else {
                 temp = temp.right;
             }
         }
         return null;
     }
 
-    public Key floor(Key key){
+    public Key floor(Key key) {
         Node temp = root;
         Key floorKey = null;
-        while (temp != null){
-            if (temp.key.compareTo(key) == 0){
+        while (temp != null) {
+            if (temp.key.compareTo(key) == 0) {
                 return temp.key;
-            }else if (temp.key.compareTo(key) < 0){
-                if (floorKey==null || floorKey.compareTo(temp.key) < 0){
+            } else if (temp.key.compareTo(key) < 0) {
+                if (floorKey == null || floorKey.compareTo(temp.key) < 0) {
                     floorKey = temp.key;
                 }
                 temp = temp.right;
-            }else{
+            } else {
                 temp = temp.left;
             }
         }
         return floorKey;
     }
 
-    public Key ceiling(Key key){
+    public Key ceiling(Key key) {
         Node temp = root;
         Key ceilingKey = null;
-        while (temp != null){
-            if (temp.key.compareTo(key) == 0){
+        while (temp != null) {
+            if (temp.key.compareTo(key) == 0) {
                 return temp.key;
-            }else if (temp.key.compareTo(key) > 0){
-                if (ceilingKey==null || ceilingKey.compareTo(temp.key) > 0){
+            } else if (temp.key.compareTo(key) > 0) {
+                if (ceilingKey == null || ceilingKey.compareTo(temp.key) > 0) {
                     ceilingKey = temp.key;
                 }
                 temp = temp.left;
-            }else{
+            } else {
                 temp = temp.right;
             }
         }
         return ceilingKey;
     }
 
-    public void deleteMin(){
-        if (root == null){
+    public void deleteMin() {
+        if (root == null) {
             return;
         }
         Node temp = root;
-        Node tempFather = root.right;
-        while (temp != null){
-            if (temp.left == null){
-                if (temp == root){
+        Stack<Node> fatherStack = new Stack<Node>();
+        fatherStack.push(root);
+        while (temp != null) {
+            if (temp.left == null) {
+                if (temp == root) {
                     root = root.right;
                     return;
-                }else{
-                    tempFather.left = temp.right;
-                    return;
+                } else {
+                    fatherStack.peek().left = temp.right;
+                    break;
                 }
-            }else{
-                tempFather = temp;
+            } else {
+                if (temp != root) {
+                    fatherStack.push(temp);
+                }
                 temp = temp.left;
             }
         }
+        while (!fatherStack.isEmpty()) {
+            Node tempFather = fatherStack.pop();
+            tempFather.count--;
+        }
     }
 
-    public void deleteMax(){
-        if (root == null){
+    public void deleteMax() {
+        if (root == null) {
             return;
         }
         Node temp = root;
-        Node tempFather = root.left;
-        while (temp != null){
-            if (temp.right == null){
-                if (temp == root){
+        Stack<Node> fatherStack = new Stack<Node>();
+        fatherStack.push(root);
+        while (temp != null) {
+            if (temp.right == null) {
+                if (temp == root) {
                     root = root.left;
                     return;
-                }else{
-                    tempFather.right = temp.left;
-                    return;
+                } else {
+                    fatherStack.peek().right = temp.left;
+                    break;
                 }
-            }else{
-                tempFather = temp;
+            } else {
+                if (temp != root) {
+                    fatherStack.push(temp);
+                }
                 temp = temp.right;
+            }
+        }
+        while (!fatherStack.isEmpty()) {
+            Node tempFather = fatherStack.pop();
+            tempFather.count--;
+        }
+    }
+
+    private Node swimToDelete(Node child, Node father) {
+        Node tempFather = father;
+        while (child.left != null && child.right != null) {
+            Node temp = child.left;
+            exchange(child.left, child, tempFather);
+            tempFather = temp;
+        }
+        return tempFather;
+    }
+
+    private void exchange(Node child, Node father, Node grandFather) {
+        Node childLeft = child.left;
+        Node childRight = child.right;
+        int childCount = child.count;
+        if (father.left == child) {
+            child.left = father;
+            child.right = father.right;
+        } else {
+            child.right = father;
+            child.left = father.left;
+        }
+        child.count = father.count - 1;
+        father.count = childCount;
+        father.left = childLeft;
+        father.right = childRight;
+        if (grandFather != null) {
+            if (grandFather.left == father) {
+                grandFather.left = child;
+            } else {
+                grandFather.right = child;
             }
         }
     }
 
-    public void delete
+    public void delete(Key key) {
+        if (root == null) {
+            return;
+        }
+        Node tempSearch = root;
+        Node tempFather = null;
+        Stack<Node> fatherStack = new Stack<>();
+        while (tempSearch != null) {
+            int compareResult = tempSearch.key.compareTo(key);
+            if (compareResult == 0) {
+                tempFather = swimToDelete(tempSearch, tempFather);
+                if (tempFather == null) {
+                    root = null;
+                } else {
+                    if (tempFather.left == tempSearch) {
+                        tempFather.left = null;
+                    } else {
+                        tempFather.right = null;
+                    }
+                }
+                break;
+            } else if (compareResult < 0) {
+                fatherStack.push(tempSearch);
+                tempFather = tempSearch;
+                tempSearch = tempSearch.right;
+            } else {
+                fatherStack.push(tempSearch);
+                tempFather = tempSearch;
+                tempSearch = tempSearch.left;
+            }
+        }
+        while (!fatherStack.isEmpty()) {
+            Node temp = fatherStack.pop();
+            temp.count--;
+        }
+    }
+
+    public Key selection(int k) {
+        if (k > size()) {
+            return null;
+        } else if (k == size()) {
+            return max();
+        } else {
+            Node temp = root;
+            int rest = k;
+            while (true) {
+                if (temp.left == null) {
+                    rest--;
+                    if (rest == 0) {
+                        return temp.key;
+                    } else {
+                        temp = temp.right;
+                    }
+                } else {
+                    if (temp.left.count < rest - 1) {
+                        rest -= temp.left.count + 1;
+                        temp = temp.right;
+                    } else if (temp.left.count == rest - 1) {
+                        return temp.key;
+                    } else {
+                        temp = temp.left;
+                    }
+                }
+            }
+        }
+    }
+
+    public int rank(Key key) {
+        if (key == null) {
+            return 0;
+        }
+        Node tempSearch = root;
+        int rank = 0;
+        while (tempSearch != null) {
+            int compareResult = tempSearch.key.compareTo(key);
+            if (compareResult == 0) {
+                if (tempSearch.left != null) {
+                    return rank + tempSearch.left.count + 1;
+                } else {
+                    return rank + 1;
+                }
+            } else if (compareResult < 0) {
+                if (tempSearch.left != null) {
+                    rank += tempSearch.left.count + 1;
+                } else {
+                    rank++;
+                }
+                tempSearch = tempSearch.right;
+            } else {
+                tempSearch = tempSearch.left;
+            }
+        }
+        return 0;
+    }
+
+    public Iterable<Key> keysPreOrder() {
+        Queue<Key> queue = new ArrayDeque<>(size());
+        if (root == null) {
+            return queue;
+        }
+        Stack<Node> keys = new Stack<>();
+        keys.push(root);
+        while (!keys.isEmpty()) {
+            Node temp = keys.pop();
+            queue.add(temp.key);
+            if (temp.right != null) {
+                keys.push(temp.right);
+            }
+            if (temp.left != null) {
+                keys.push(temp.left);
+            }
+        }
+        return queue;
+    }
+
+    public Iterable<Key> keysInOrder() {
+        Queue<Key> queue = new ArrayDeque<>(size());
+        if (root == null) {
+            return queue;
+        }
+        Stack<Node> keys = new Stack<>();
+        Node p = root;
+        while (!keys.isEmpty() || p != null) {
+            if (p != null) {
+                keys.push(p);
+                p = p.left;
+            } else {
+                p = keys.pop();
+                queue.add(p.key);
+                p = p.right;
+            }
+        }
+        return queue;
+    }
 }
 
 
--- src/com/algorithm/chapter3/searching/BinarySearchTreeTest.java
+++ src/com/algorithm/chapter3/searching/BinarySearchTreeTest.java
@@ -1,18 +1,22 @@
 package com.algorithm.chapter3.searching;
 
+import com.sun.jmx.remote.internal.ArrayQueue;
 import org.junit.Test;
+
+import java.util.ArrayDeque;
 
 /**
  * @author XiaoHan
  */
 public class BinarySearchTreeTest {
-    public BinarySearchTree<String, Integer> initTestData(String[] keys, Integer[] values){
+    public BinarySearchTree<String, Integer> initTestData(String[] keys, Integer[] values) {
         BinarySearchTree<String, Integer> binarySearchTree = new BinarySearchTree<String, Integer>();
         for (int i = 0; i < keys.length; i++) {
             binarySearchTree.put(keys[i], values[i]);
         }
         return binarySearchTree;
     }
+
     @Test
     public void testPutAndGetValue() {
         String[] strings = {"a", "b", "d", "c", "z", "k", "g"};
@@ -27,7 +31,7 @@ public class BinarySearchTreeTest {
     }
 
     @Test
-    public void testSize(){
+    public void testSize() {
         assert (new BinarySearchTree<String, Integer>()).size() == 0;
         String[] strings = {"a", "b", "d", "c", "z", "k", "g"};
         Integer[] integers = {1, 20, 13, 42, 57, 16, 7};
@@ -36,7 +40,7 @@ public class BinarySearchTreeTest {
     }
 
     @Test
-    public void testMinMax(){
+    public void testMinMax() {
         assert (new BinarySearchTree<String, Integer>()).min() == null;
         assert (new BinarySearchTree<String, Integer>()).max() == null;
         String[] strings = {"m", "b", "d", "c", "z", "k", "g"};
@@ -47,7 +51,7 @@ public class BinarySearchTreeTest {
     }
 
     @Test
-    public void testFloorAndCeiling(){
+    public void testFloorAndCeiling() {
         assert (new BinarySearchTree<String, Integer>()).floor("d") == null;
         assert (new BinarySearchTree<String, Integer>()).ceiling("d") == null;
         String[] strings = {"m", "b", "d", "c", "y", "k", "g"};
@@ -57,7 +61,6 @@ public class BinarySearchTreeTest {
         assert binarySearchTree.floor("e").equals("d");
         assert binarySearchTree.floor("x").equals("m");
         assert binarySearchTree.floor("a") == null;
-
         assert binarySearchTree.ceiling("c").equals("c");
         assert binarySearchTree.ceiling("e").equals("g");
         assert binarySearchTree.ceiling("x").equals("y");
@@ -65,38 +68,152 @@ public class BinarySearchTreeTest {
     }
 
     @Test
-    public void testDeleteMin(){
+    public void testDeleteMin() {
         String[] strings = {"m", "b", "d", "c", "z", "k", "g"};
         Integer[] integers = {1, 20, 13, 42, 57, 16, 7};
         BinarySearchTree binarySearchTree = initTestData(strings, integers);
+        assert binarySearchTree.size() == 7;
         binarySearchTree.deleteMin();
+        assert binarySearchTree.size() == 6;
         assert binarySearchTree.min().equals("c");
         binarySearchTree.deleteMin();
+        assert binarySearchTree.size() == 5;
         assert binarySearchTree.min().equals("d");
         String[] strings1 = {"a"};
         Integer[] integers1 = {3};
         BinarySearchTree binarySearchTree1 = initTestData(strings1, integers1);
         binarySearchTree1.deleteMin();
+        assert binarySearchTree1.size() == 0;
         assert binarySearchTree1.min() == null;
         binarySearchTree1.deleteMin();
+        assert binarySearchTree1.size() == 0;
         assert binarySearchTree1.min() == null;
     }
 
     @Test
-    public void testDeleteMax(){
+    public void testDeleteMax() {
         String[] strings = {"m", "b", "d", "c", "z", "k", "g"};
         Integer[] integers = {1, 20, 13, 42, 57, 16, 7};
         BinarySearchTree binarySearchTree = initTestData(strings, integers);
+        assert binarySearchTree.size() == 7;
         binarySearchTree.deleteMax();
+        assert binarySearchTree.size() == 6;
         assert binarySearchTree.max().equals("m");
         binarySearchTree.deleteMax();
+        assert binarySearchTree.size() == 5;
         assert binarySearchTree.max().equals("k");
         String[] strings1 = {"a"};
         Integer[] integers1 = {3};
         BinarySearchTree binarySearchTree1 = initTestData(strings1, integers1);
+        assert binarySearchTree1.size() == 1;
         binarySearchTree1.deleteMax();
+        assert binarySearchTree1.size() == 0;
         assert binarySearchTree1.max() == null;
         binarySearchTree1.deleteMax();
         assert binarySearchTree1.max() == null;
+    }
+
+    @Test
+    public void testDelete() {
+        String[] strings = {"m", "b", "d", "c", "z", "k", "g"};
+        Integer[] integers = {1, 20, 13, 42, 57, 16, 7};
+        BinarySearchTree binarySearchTree = initTestData(strings, integers);
+        assert binarySearchTree.size() == 7;
+        binarySearchTree.delete("d");
+        assert binarySearchTree.getValue("d") == null;
+        assert binarySearchTree.size() == 6;
+        binarySearchTree.delete("z");
+        assert binarySearchTree.size() == 5;
+        assert binarySearchTree.max().equals("m");
+        assert binarySearchTree.getValue("z") == null;
+        binarySearchTree.delete("c");
+        assert binarySearchTree.size() == 4;
+        assert binarySearchTree.getValue("c") == null;
+        String[] strings1 = {"a"};
+        Integer[] integers1 = {3};
+        BinarySearchTree binarySearchTree1 = initTestData(strings1, integers1);
+        binarySearchTree1.delete("a");
+        assert binarySearchTree1.getValue("a") == null;
+        assert binarySearchTree1.size() == 0;
+        binarySearchTree1.delete("a");
+        assert binarySearchTree1.getValue("a") == null;
+        assert binarySearchTree1.size() == 0;
+    }
+
+    @Test
+    public void testSelection() {
+        String[] strings = {"m", "b", "d", "c", "z", "k", "g"};
+        Integer[] integers = {1, 20, 13, 42, 57, 16, 7};
+        BinarySearchTree binarySearchTree = initTestData(strings, integers);
+        assert binarySearchTree.selection(1).equals("b");
+        assert binarySearchTree.selection(2).equals("c");
+        assert binarySearchTree.selection(3).equals("d");
+        assert binarySearchTree.selection(4).equals("g");
+        assert binarySearchTree.selection(5).equals("k");
+        assert binarySearchTree.selection(6).equals("m");
+        assert binarySearchTree.selection(7).equals("z");
+        String[] strings1 = {"a"};
+        Integer[] integers1 = {3};
+        BinarySearchTree binarySearchTree1 = initTestData(strings1, integers1);
+        assert binarySearchTree1.selection(1).equals("a");
+        binarySearchTree1.delete("a");
+        assert binarySearchTree1.selection(1) == null;
+    }
+
+    @Test
+    public void testRank() {
+        String[] strings = {"m", "b", "d", "c", "z", "k", "g"};
+        Integer[] integers = {1, 20, 13, 42, 57, 16, 7};
+        BinarySearchTree binarySearchTree = initTestData(strings, integers);
+        assert binarySearchTree.rank("b") == 1;
+        assert binarySearchTree.rank("c") == 2;
+        assert binarySearchTree.rank("d") == 3;
+        assert binarySearchTree.rank("g") == 4;
+        assert binarySearchTree.rank("k") == 5;
+        assert binarySearchTree.rank("m") == 6;
+        assert binarySearchTree.rank("z") == 7;
+        assert binarySearchTree.rank(null) == 0;
+        String[] strings1 = {"a"};
+        Integer[] integers1 = {3};
+        BinarySearchTree binarySearchTree1 = initTestData(strings1, integers1);
+        assert binarySearchTree1.rank("a") == 1;
+        binarySearchTree1.delete("a");
+        assert binarySearchTree1.rank("a") == 0;
+    }
+
+    @Test
+    public void testKeysPreOrder(){
+        String[] strings = {"m", "b", "d", "c", "z", "k", "g"};
+        Integer[] integers = {1, 20, 13, 42, 57, 16, 7};
+        BinarySearchTree binarySearchTree = initTestData(strings, integers);
+        ArrayDeque<String> queue = (ArrayDeque<String>) binarySearchTree.keysPreOrder();
+        String[] preOrder = {"m", "b", "d", "c", "k", "g", "z"};
+        for (int i=0; i<strings.length; i++){
+            assert  queue.pop().equals(preOrder[i]);
+        }
+        String[] strings1 = {"a"};
+        Integer[] integers1 = {3};
+        BinarySearchTree binarySearchTree1 = initTestData(strings1, integers1);
+        binarySearchTree1.delete("a");
+        queue = (ArrayDeque<String>) binarySearchTree1.keysPreOrder();
+        assert queue.size() == 0;
+    }
+
+    @Test
+    public void testKeysInOrder(){
+        String[] strings = {"m", "b", "d", "c", "z", "k", "g"};
+        Integer[] integers = {1, 20, 13, 42, 57, 16, 7};
+        BinarySearchTree binarySearchTree = initTestData(strings, integers);
+        ArrayDeque<String> queue = (ArrayDeque<String>) binarySearchTree.keysInOrder();
+        String[] preOrder = {"b", "c", "d", "g", "k", "m", "z"};
+        for (int i=0; i<strings.length; i++){
+            assert  queue.pop().equals(preOrder[i]);
+        }
+        String[] strings1 = {"a"};
+        Integer[] integers1 = {3};
+        BinarySearchTree binarySearchTree1 = initTestData(strings1, integers1);
+        binarySearchTree1.delete("a");
+        queue = (ArrayDeque<String>) binarySearchTree1.keysInOrder();
+        assert queue.size() == 0;
     }
 }
